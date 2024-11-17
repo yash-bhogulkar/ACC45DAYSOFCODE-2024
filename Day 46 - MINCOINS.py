d6f0664d0a8317f2fa8e4087caa311bff71dc54b
Yash Bhogulkar <yashbhogulkar15@gmail.com>
2024-11-17 15:37:55 +0530

    Add files via upload

---
+++ Day 46 - MINCOINS.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t = int(input())
+for i in range(t):
+    x = int(input())
+    if x%10 == 0:
+        print(x//10)
+    elif x%10 != 0 and x%5 == 0:
+        print((x//10)+1)
+    else:
+        print(-1)
